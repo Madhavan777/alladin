72250f6241754d642a2aa8adb8df8d7d4541fd21
Madhavan777 <84850806+Madhavan777@users.noreply.github.com>
2021-08-08 18:14:22 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,23 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+function clues(){
+    fill("yellow");
+    textSize(18);
+    text("Johan",100,50);
+    fill("green");
+    text("Hint:always changing, not constant",100,70);
+
+    fill("yellow");
+    textSize(18);
+    text("Madhavan",700,150);
+    fill("green");
+    text("Hint:it performs a particular task",700,170);
+    
+    fill("yellow");
+    textSize(18);
+    text("Sam",100,250);
+    fill("green");
+    text("Hint:stores all information",100,270);
+}
+
